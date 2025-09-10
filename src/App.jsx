dbc3d1e186e9c170a0dd0894630968670ae6f451
Shobhit Srivastava <shobhitsri1405@gmail.com>
2025-09-10 13:20:07 +0530

    Implement routing and add ice cream pages with navigation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Strawberry from './pages/StrawBerry'
+import BlueBerry from './pages/BlueBerry'
+import Chocolate from './pages/Chocolate'
+import EverGreen from './pages/EverGreen'
+import NotFound from './pages/NotFound'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path='/' element={<Strawberry />} />
+      <Route path='/strawberry' element={<Strawberry />} />
+      <Route path='/blueberry' element={<BlueBerry />} />
+      <Route path='/chocolate' element={<Chocolate />} />
+      <Route path='/evergreen' element={<EverGreen />} />
+      <Route path='*' element={<NotFound />} />
+
+    </Routes>
   )
 }
 
