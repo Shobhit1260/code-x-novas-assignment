35afa160302b10a47a488311052561a4686595a1
Shobhit Srivastava <shobhitsri1405@gmail.com>
2025-09-10 18:50:43 +0530

    fix

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -14,10 +14,10 @@ const Options = () => {
 
         <div className="relative">
 
-           <Link
+        <Link
         to="/strawberry"
-        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[130px]  scale-120 max-md:scale-110  "
-           >
+        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[110px]  scale-120 max-md:scale-110  "
+        >
         <img
           src={pic1}
           alt="ice-cream2"
@@ -32,9 +32,9 @@ const Options = () => {
      
         <div className="relative">
 
-           <Link
+        <Link
         to="/blueberry"
-        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[130px]  scale-120 max-md:scale-110  "
+        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[110px]  scale-120 max-md:scale-110  "
            >
         <img
           src={pic2}
@@ -52,7 +52,7 @@ const Options = () => {
 
         <Link
         to="/chocolate"
-        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[130px]  scale-120 max-md:scale-110  "
+        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[110px]  scale-120 max-md:scale-110  "
            >
         <img
           src={pic3}
@@ -65,21 +65,18 @@ const Options = () => {
         
         <div className="relative">
 
-           <Link
+      <Link
         to="/evergreen"
-        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[130px]  scale-120 max-md:scale-110  "
-           >
+        className=" h-[10vh] py-16 relative  z-10 flex overflow-hidden items-baseline justify-end  w-[110px]  scale-120 max-md:scale-110  "
+        >
         <img
           src={pic4}
           alt="ice-cream2"
           className="h-[18vh]  mt-12 scale-80 absolute -top-[1.2rem] z-40 transition-transform duration-500 ease-in-out rotate-[-15deg] hover:scale-110 hover:rotate-[15deg]"
         />
-
       </Link>
-
       <span className="  w-[62%] max-xl:w-[53%]  max-sm:w-[55%] h-[30%] rounded-lg border absolute border-t-0  -bottom-1.5 left-[28px] max-xl:left-[35px] max-md:left-[24px] scale-[140%]"></span>
-
-        </div>
+      </div>
      </div>
 
 
